3bf1a5eeb40f750af9effbce1ab242d78bc51629
refresh <fly2nd@gmail.com>
2022-04-07 17:15:07 +0900

    04-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({fav}) {
+  return <h1>I like {fav}</h1>;
+}
+
+const foodILike = [
+  {
+    name: 'Kimchi',
+    image: 'https://www.maangchi.com/wp-content/uploads/2019/11/vegetarian-kimchi.jpg',
+  }, {
+    name: 'Samgyeopsal',
+    image: 'https://www.maangchi.com/wp-content/uploads/2019/11/vegetarian-kimchi.jpg',
+  }, {
+    name: 'Bibimbap',
+    image: 'https://www.maangchi.com/wp-content/uploads/2019/11/vegetarian-kimchi.jpg',
+  }, {
+    name: 'Doncasu',
+    image: 'https://www.maangchi.com/wp-content/uploads/2019/11/vegetarian-kimchi.jpg',
+  }, {
+    name: 'Kimbap',
+    image: 'https://www.maangchi.com/wp-content/uploads/2019/11/vegetarian-kimchi.jpg',
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
     </div>
   );
 }
