6812f60ad2b44e9d7a9c7ec1df255a2fbc9fddbc
refresh <fly2nd@gmail.com>
2022-04-08 12:28:47 +0900

    05-3

--- src/App.js
+++ src/App.js
@@ -1,83 +1,37 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>;
-      <h4>{rating}/5.0</h4>
-      <img src={picture} width="300" alt={name} />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('hello');
+  }
+
+  state = {
+    count: 0,
+  };
+
+  add = () => {
+    this.setState( current => ({
+      count: current.count + 1,
+    }));
+  };
+
+  minus = () => {
+    this.setState( current => ({
+      count: current.count - 1,
+    }));
+  };
+
+  render() {
+    console.log('render');
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
-
-const foodILike = [
-  {
-    id: 1,
-    name: 'Kimchi',
-    image: 'https://www.maangchi.com/wp-content/uploads/2019/11/vegetarian-kimchi.jpg',
-    rating: 5,
-  }, {
-    id: 2,
-    name: 'Samgyeopsal',
-    image: 'https://www.maangchi.com/wp-content/uploads/2019/11/vegetarian-kimchi.jpg',
-    rating: 4.9,
-  }, {
-    id: 3,
-    name: 'Bibimbap',
-    image: 'https://www.maangchi.com/wp-content/uploads/2019/11/vegetarian-kimchi.jpg',
-    rating: 4.8,
-  }, {
-    id: 4,
-    name: 'Doncasu',
-    image: 'https://www.maangchi.com/wp-content/uploads/2019/11/vegetarian-kimchi.jpg',
-    rating: 4.7,
-  }, {
-    id: 5,
-    name: 'Kimbap',
-    image: 'https://www.maangchi.com/wp-content/uploads/2019/11/vegetarian-kimchi.jpg',
-    rating: 4.6,
-  },
-];
-
-/*
-const renderFood = (dish) => (
-  <Food
-    name={dish.name}
-    picture={dish.image}
-  />
-);
-
-
-function renderFood(dish) {
-  return (
-    <Food
-      name={dish.name}
-      picture={dish.image}
-    />
-  );
-}
-*/
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
 
 export default App;
